884dcdc3774b7eee382929dfc784118147709dac
DESKTOP-PPC0C2S\Elijah <carlsoel@sheridancollege.ca>
2023-03-08 13:07:25 -0500

    Added Ai pathing and old man animator

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -6,16 +6,37 @@ using UnityEngine.AI;
 public class AIMovement : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public Transform target;
+    private Rigidbody rb;
+    private Animator animator;
+    private Transform[] waypoints;
+    private bool reachedWaypoint = true;
+    private Transform randomTarget;
+    public float radiusRange = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        agent.SetDestination(target.position);
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+        GameObject parentWaypoint = GameObject.FindWithTag("Waypoints");
+        waypoints = parentWaypoint.GetComponentsInChildren<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float velocity = Mathf.Abs(rb.velocity.magnitude);
+        animator.SetFloat("Velocity", velocity);
+            if (reachedWaypoint == true)
+        {
+            int randomIndex = Random.Range(0, waypoints.Length);
+            randomTarget = waypoints[randomIndex];
+            agent.SetDestination(randomTarget.position);
+            reachedWaypoint = false;
+        }
+        float distance = Vector3.Distance(transform.position, randomTarget.position);
+        if (distance <= radiusRange)
+        {
+            reachedWaypoint = true;
+        }
     }
 }
