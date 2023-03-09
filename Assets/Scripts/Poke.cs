f32dffb7e543bfafc45634f8a2cc2ca0c3445852
Joel Bergen <80221062+JoelGameDesign@users.noreply.github.com>
2023-03-09 09:27:32 -0500

    Douglas fixed everything
    Douglas fixed the execution order
    now everything works

--- Assets/Scripts/Poke.cs
+++ Assets/Scripts/Poke.cs
@@ -1,25 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Poke : MonoBehaviour
 {
     //[HideInInspector]
     public bool poked = false;
     // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public UnityEvent OnPoke;
 
     public void PokeObject()
     {
         poked = true;
+        OnPoke?.Invoke();
     }
 }
