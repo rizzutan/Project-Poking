c2fc6fa8455a68d32ecbd3f87adc43be57dc06e8
Joel Bergen <80221062+JoelGameDesign@users.noreply.github.com>
2023-03-09 14:06:19 -0500

    Added destructible pillar
    - Added pillar
    - Can be destroyed

--- Assets/Scripts/Pokeables/SwapStatePoke.cs
+++ Assets/Scripts/Pokeables/SwapStatePoke.cs
@@ -8,6 +8,15 @@ public class SwapStatePoke : MonoBehaviour
     [SerializeField] int maxPokeCount;
     [SerializeField] GameObject objectSwap;
 
+    AudioController ac;
+    [SerializeField] string soundName;
+    [SerializeField] bool randomSound;
+
+    void Start()
+    {
+        ac = GetComponent<AudioController>();
+    }
+
     public void swapPokeState()
     {
         pokeCount += 1;
@@ -17,6 +26,28 @@ public class SwapStatePoke : MonoBehaviour
             GameObject newGameObject = Instantiate(objectSwap, transform.position, Quaternion.identity);
             newGameObject.transform.rotation = transform.rotation;
             Destroy(gameObject);
+        } else
+        {
+            TryPlaySound();
+        }
+    }
+
+    void TryPlaySound()
+    {
+        if ((soundName == null || soundName == "") && !randomSound)
+        {
+            return;
+        }
+        else
+        {
+            if (randomSound)
+            {
+                ac.PlayRandomAudio();
+            }
+            else
+            {
+                ac.PlayAudio(soundName);
+            }
         }
     }
 }
